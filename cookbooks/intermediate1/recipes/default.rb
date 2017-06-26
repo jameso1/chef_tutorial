82b2f70ae2812c6747c8faabbf8c79fce49976d2
jameso1 <jodonnell@offis.com.au>
2017-06-26 05:11:56 +0000

    Added a hello world message to the default.rb recipe

---
+++ cookbooks/intermediate1/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: intermediate1
+# Recipe:: default
+#
+# Copyright 2017, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Hello World! You should try Chef with RightScale!"
